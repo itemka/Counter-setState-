f7b27ed6ce3766fffa8b06ae204783c352c1d290
Артем Павленко <itemka@MacBook-Pro-Artem.local>
2019-08-29 00:33:52 +0300

    Add button, ShowNumber and localState

--- src/App.js
+++ src/App.js
@@ -1,48 +1,22 @@
 import React from 'react'
 import './App.css';
-import Display from "./Components/Display";
-import Buttons from "./Components/Buttons";
+import Button from "./Components/Button/Button";
+import ShowNumber from "./Components/ShowNumber/ShowNumber";
 
 class App extends React.Component {
 
     state = {
         number: 0,
         buttons: [
-            {title: '+', isDone: true},
-            {title: '-', isDone: false},
+            {buttonsTitle: '+'},
         ],
-        limit: {number: 0, T: 3, B: -3, error: 'Stop'}
-    };
-
-    onClickButtonsPlusMinus = isDone => {
-        if (isDone) {
-            this.state.number = this.state.limit.number;
-
-            if (this.state.limit.number < this.state.limit.T) {
-                this.setState({number: this.state.number + 1});
-                this.state.limit.number = this.state.limit.number + 1;
-                console.log(this.state.limit.number);
-            } else {
-                this.setState({number: this.state.limit.error});
-            }
-        } else if (!isDone) {
-            this.state.number = this.state.limit.number;
-
-            if (this.state.limit.number > this.state.limit.B) {
-                this.setState({number: this.state.number - 1});
-                this.state.limit.number = this.state.limit.number - 1;
-                console.log(this.state.limit.number);
-            } else {
-                this.setState({number: this.state.limit.error});
-            }
-        }
     };
 
     render = () => {
         return (
             <div>
-                <Display number={this.state.number}/>
-                <Buttons onClickButtonsPlusMinus={this.onClickButtonsPlusMinus} buttons={this.state.buttons}/>
+                <ShowNumber number={this.state.number}/>
+                <Button onClickButton={()=>{}} buttonsTitle={this.state.buttons[0].buttonsTitle}/>
             </div>
         );
     }
@@ -50,3 +24,28 @@ class App extends React.Component {
 
 export default App;
 
+// Побаловался
+// limit: {number: 0, T: 3, B: -3, error: 'Stop'}
+// onClickButton = isDone => {
+//     if (isDone) {
+//         this.state.number = this.state.limit.number;
+//
+//         if (this.state.limit.number < this.state.limit.T) {
+//             this.setState({number: this.state.number + 1});
+//             this.state.limit.number = this.state.limit.number + 1;
+//             console.log(this.state.limit.number);
+//         } else {
+//             this.setState({number: this.state.limit.error});
+//         }
+//     } else if (!isDone) {
+//         this.state.number = this.state.limit.number;
+//
+//         if (this.state.limit.number > this.state.limit.B) {
+//             this.setState({number: this.state.number - 1});
+//             this.state.limit.number = this.state.limit.number - 1;
+//             console.log(this.state.limit.number);
+//         } else {
+//             this.setState({number: this.state.limit.error});
+//         }
+//     }
+// };
---
+++ src/Components/ShowNumber/ShowNumber.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ShowNumber extends React.Component {
+    render() {
+        return (
+            <>
+                {this.props.number}
+            </>
+        )
+    }
+}
+export default ShowNumber;
