b6966c280c774c1b7d2ca1bffcaa8b6e6ee5f491
itemka <itemka2503@gmail.com>
2019-09-28 18:14:58 +0300

    test add of commit without node_modules

--- src/App.js
+++ src/App.js
@@ -164,30 +164,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// Побаловался
-
-// limit: {number: 0, T: 3, B: -3, error: 'Stop'}
-// onClickButton = isDone => {
-//     if (isDone) {
-//         this.state.number = this.state.limit.number;
-//
-//         if (this.state.limit.number < this.state.limit.T) {
-//             this.setState({number: this.state.number + 1});
-//             this.state.limit.number = this.state.limit.number + 1;
-//             console.log(this.state.limit.number);
-//         } else {
-//             this.setState({number: this.state.limit.error});
-//         }
-//     } else if (!isDone) {
-//         this.state.number = this.state.limit.number;
-//
-//         if (this.state.limit.number > this.state.limit.B) {
-//             this.setState({number: this.state.number - 1});
-//             this.state.limit.number = this.state.limit.number - 1;
-//             console.log(this.state.limit.number);
-//         } else {
-//             this.setState({number: this.state.limit.error});
-//         }
-//     }
-// };
