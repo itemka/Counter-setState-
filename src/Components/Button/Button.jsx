50a2b69275cc93bde8990db608d1d81cd1e97c80
itemka <itemka2503@gmail.com>
2020-01-24 15:40:08 +0300

    Code refactor <ENTER>

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,21 +2,8 @@ import React from 'react';
 import css from './Button.module.css';
 
 const Button = props => {
-    // let state = {
-    //     mouseDown: false,
-    // };
-    // let onMouseDown=(event)=>{
-    //     if (event.onmousedown){
-    //         state.mouseDown = true;
-    //     }
-    //     else{
-    //         state.mouseDown = false;
-    //     }
-    // };
-
     let disabledButtonCSS = props.access === false ? `${css.disabledButton}` : ``;
     let disabledButton = props.access === false ? `disabled` : ``;
-    // let mouseDown = state.mouseDown === false ? `` : `${css.mousDown}`;
 
     return (
         <>
